dbfdb463caff4187aab6755141d513d106dfed37
Gries, Matt <Matt.Gries@freedompay.com>
2019-08-18 21:05:35 -0400

    Moved StartUp functionality to Program

--- MVCTesting/src/com/freedompay/application/Program.java
+++ MVCTesting/src/com/freedompay/application/Program.java
@@ -1,10 +1,31 @@
 package com.freedompay.application;
 
+import javax.swing.SwingUtilities;
+import com.freedompay.components.MainMenu;
 import com.freedompay.configuration.*;
 
 public class Program {
 	
-	public static void main(String[] args) {
-		StartUp.runApplication();
+	private static BaseView baseView;
+	private static MainMenu menu;
+	private static RouteConfig router;
+	
+	public static void main(String[] args) {	
+		Configure.setConfigurations();
+		
+		router = new RouteConfig();
+		baseView = new BaseView();
+		menu = new MainMenu();
+		
+		router.addObserver(baseView);
+		menu.addObserver(router);
+		baseView.init(menu);
+		router.update("Home");
+		
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				baseView.createAndShowGUI(Configure.getVPWidth(), Configure.getVPHeight());
+			}
+		});
 	}
 }
